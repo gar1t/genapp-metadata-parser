d1a8ca736f8c767721370b165d98e5f05b9f0a55
Benjamin Savoy <thefrozenanthem@gmail.com>
2013-03-11 17:12:00 +0100

    now correctly returning env

--- parser.py
+++ parser.py
@@ -17,34 +17,38 @@ def get_metadata():
     for key in data.keys():
         val = data[key]
         if key == "app":
-            resource_arr = val["resources"]
-            services = {}
-            resources = {}
-            for entry in resource_arr:
-                name = entry.get("name", None)
-                if name == None:
-                    service = entry["service"]
-                    del entry["service"]
-                    services[service] = entry
+            for key2 in val.keys():
+                val2 = val[key2]
+                if key2 == "resources":
+                    services = {}
+                    resources = {}
+                    for entry in val2:
+                        name = entry.get("name", None)
+                        if name == None:
+                            service = entry["service"]
+                            del entry["service"]
+                            services[service] = entry
+                        else:
+                            type = entry["type"]
+                            if type == "datasource":
+                                type = "database"
+                            if type == "database":
+                                config = entry["config"]
+                                url = config["DATABASE_URL"]
+                                db = re.sub(r'.*\/', '', url)
+                                host = re.sub(r'^.*mysql:\/\/', '', url)
+                                host = re.sub(r':[0-9]*\/%s'%db, '', host)
+                                port = re.sub(r'^.*mysql:\/\/%s:'%host, '', url)
+                                port = re.sub(r'\/' + db, '', port)
+                                config["DATABASE_DB"] = db
+                                config["DATABASE_HOST"] = host
+                                config["DATABASE_PORT"] = port
+                            if resources.get(type, None) == None:
+                                resources[type] = {}
+                            del entry["name"]
+                            resources[type][name] = entry
                 else:
-                    type = entry["type"]
-                    if type == "datasource":
-                        type = "database"
-                    if type == "database":
-                        config = entry["config"]
-                        url = config["DATABASE_URL"]
-                        db = re.sub(r'.*\/', '', url)
-                        host = re.sub(r'^.*mysql:\/\/', '', url)
-                        host = re.sub(r':[0-9]*\/%s'%db, '', host)
-                        port = re.sub(r'^.*mysql:\/\/%s:'%host, '', url)
-                        port = re.sub(r'\/' + db, '', port)
-                        config["DATABASE_DB"] = db
-                        config["DATABASE_HOST"] = host
-                        config["DATABASE_PORT"] = port
-                    if resources.get(type, None) == None:
-                        resources[type] = {}
-                    del entry["name"]
-                    resources[type][name] = entry
+                    metadata[key2] = val2
             if services != {}:
                 metadata["services"] = services
             if resources != {}:
@@ -80,7 +84,7 @@ def get_values(args, options):
 
 if __name__ == "__main__":
     get_metadata()
-
+    
     parser = OptionParser() 
     parser.add_option("-d", "--default", 
         help="Returns default value if search fails")
